fd40a1f13aca156a514acdfab794f9018034a376
danielcristancho <daniel.cristancho@klym.com>
2024-10-07 17:51:27 -0500

    New method of page@

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/autorizacion-tratamiento-datos.html")
+def autorizacion():
+    return render_template("autorizacion-tratamiento-datos.html")
+
+
+@app.route("/politica-tratamiento-datos.html")
+def ploitica():
+    return render_template("politica-tratamiento-datos.html")
+
+
+@app.route("/aviso-privacidad.html")
+def aviso():
+    return render_template("aviso-privacidad.html")
+
+
+app.run()
